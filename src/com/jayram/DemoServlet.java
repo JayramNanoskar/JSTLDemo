bb8596349193c6d245283851ef3876e93487428b
JayramNanoskar <jayram.n978@gmail.com>
2020-09-01 22:13:23 +0530

    initial commit

---
+++ src/com/jayram/DemoServlet.java
@@ -0,0 +1,18 @@
+package com.jayram;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class DemoServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest req , HttpServletResponse res) throws ServletException, IOException {
+		
+		
+	}
+}
