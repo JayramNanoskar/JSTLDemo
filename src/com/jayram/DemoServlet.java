fd72ee4e27f52c401e1fe36e8bb9eab265425c35
JayramNanoskar <jayram.n978@gmail.com>
2020-09-01 22:20:12 +0530

    using EL inside view

--- src/com/jayram/DemoServlet.java
+++ src/com/jayram/DemoServlet.java
@@ -9,10 +9,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
+@WebServlet(urlPatterns = "/")
 public class DemoServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req , HttpServletResponse res) throws ServletException, IOException {
+		String name = "jayram";
+		req.setAttribute("name", name);
 		
-		
+		RequestDispatcher rd = req.getRequestDispatcher("display.jsp");
+		rd.forward(req, res);
 	}
 }
